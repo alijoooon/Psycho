5dfe89f09b8ed8a1472907148f3cc50a7b8512c3
alijoooon <djalijoooon@gmail.com>
2016-06-05 04:26:03 -0700

    Create psycho.lua

---
+++ plugins/psycho.lua
@@ -0,0 +1,13 @@
+do
+function run(msg, matches)
+  return " psycho V5 Based On Lua \n\n Developer: @psycho_TG \n Channel: @psycho_team \n\n WebSite: Www.nootface.ir \n\n /psycho\n\n\n Picture: http://s7.picofile.com/file/8254437226/photo_2016_05_31_05_58_11.jpg"
+  end
+return {
+  description = " psycho ", 
+  usage = " psycho ",
+  patterns = {
+    "^[#/!][Pp]sycho$",
+  },
+  run = run
+}
+end
